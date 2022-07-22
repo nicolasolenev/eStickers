e7629bbfa0e7d070bc5dfd99a8abdb6a780e445f
Nicolas Olenev <cardinal2113@gmail.com>
2022-07-22 12:44:35 +0300

    add button and Chrome color picker

--- src/components/palette/palette.jsx
+++ src/components/palette/palette.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { HuePicker, AlphaPicker } from 'react-color';
+import { ChromePicker } from 'react-color';
 
 import { setPaletteValue } from '../../store/settingsSlice';
 import { changeColor } from '../../store/devicesSlice';
@@ -10,6 +10,7 @@ import Themes from './themes';
 export default function Palette() {
   const dispatch = useDispatch();
   const [color, setColor] = useState({ h: 200, s: 0.8, l: 0.53 });
+  const [isOpen, setOpen] = useState(false);
   const settings = useSelector((state) => state.settings);
 
   const handleChange = (color) => {
@@ -27,20 +28,18 @@ export default function Palette() {
     <div className="palette">
       <Buttons setColor={setColor} />
 
-      <div className="palette__settings palette__settings-picker">
-        {/* <ChromePicker
-          color={color}
-          onChange={(color) => {
-            handleChange(color);
-          }}
-        /> */}
-        <HuePicker
-          color={color}
-          onChange={(color) => {
-            handleChange(color);
-          }}
-        />
-        <AlphaPicker
+      <button
+        className="palette__colorpicker-button"
+        onClick={() => setOpen(!isOpen)}
+      >
+        Color Picker
+      </button>
+
+      <div
+        className="palette__settings palette__settings-picker"
+        style={{ display: isOpen ? 'block' : 'none' }}
+      >
+        <ChromePicker
           color={color}
           onChange={(color) => {
             handleChange(color);
